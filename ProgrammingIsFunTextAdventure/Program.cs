581ee149fa758870a3c5bc328d320e3295c7c548
Eggo <63312123+EggoGF@users.noreply.github.com>
2020-04-19 11:10:40 -0700

    Add project files.

---
+++ ProgrammingIsFunTextAdventure/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ProgrammingIsFunTextAdventure
+{
+    class Program
+    {
+        static void Main()
+        {
+            string CharacterName = "John Doe";
+
+            Console.WriteLine("Welcome to Quarantine Explorer!");
+            Console.WriteLine("What is your name, adventurer?");
+
+            CharacterName = Console.ReadLine();
+            Console.WriteLine("Excellent!  You are known as " + CharacterName + " the Great.  The bards will sing your tales for decades to come.");
+        }
+    }
+}
